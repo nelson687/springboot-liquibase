d618d8d9b44c8ff3202f4323999410e8b919192b
Nelson Gallardo <nelson.gallardo@secretescapes.com>
2018-10-16 15:09:51 +0100

    uploading changelog

--- src/main/resources/db/changelog/changelog.groovy
+++ src/main/resources/db/changelog/changelog.groovy
@@ -1,4 +1,12 @@
 databaseChangeLog {
-
+  changeSet(id: '1539698971671-1', author: 'nelson (generated)') {
+    createTable(tableName: 'TestTable') {
+      column(name: 'id', type: 'BIGINT', autoIncrement: true) {
+        constraints(primaryKey: true, primaryKeyName: 'TestTablePK')
+      }
+      column(name: 'anotherProperty', type: 'VARCHAR(255)')
+      column(name: 'name', type: 'VARCHAR(255)')
+    }
+  }
 
 }
